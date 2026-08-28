7e6c8e011170bedce1c303822049d9e249b71ca0
Ruslan <info@ruslandev.uz>
2026-08-28 07:15:50 -0400

    fix: add goose annotations to migrations

--- migrations/00005_add_salary_text.sql
+++ migrations/00005_add_salary_text.sql
@@ -1,1 +1,5 @@
+-- +goose Up
 ALTER TABLE vacancies ADD COLUMN salary_text VARCHAR(255);
+
+-- +goose Down
+ALTER TABLE vacancies DROP COLUMN salary_text;
--- migrations/00006_create_bot_texts.sql
+++ migrations/00006_create_bot_texts.sql
@@ -1,4 +1,8 @@
+-- +goose Up
 CREATE TABLE IF NOT EXISTS bot_texts (
     id VARCHAR(50) PRIMARY KEY,
     text_content TEXT NOT NULL
 );
+
+-- +goose Down
+DROP TABLE IF EXISTS bot_texts;
